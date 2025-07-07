4587070a9063e0bbd489f4967a48af435efac7d5
splatoonkurochan <splatoonkurochan74@gmail.com>
2025-07-07 19:58:50 +0900

    2025.07.07

---
+++ chap2/chap2-8.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+# webページを取得して解析する
+load_url = "https://www.ymori.com/books/python2nen/test2.html"
+html = requests.get(load_url)
+soup = BeautifulSoup(html.content, "html.parser")
+
+# すべてのaタグを検索して、リンクを表示する
+for element in soup.find_all("a"):
+    print(element.text)
+    url = element.get("href")
+    print(url)
+    
